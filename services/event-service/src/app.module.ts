c184b5ffdaf827762dc7c217b8f9b1bae7ea1992
Melisaates <101855839+Melisaates@users.noreply.github.com>
2025-12-12 16:29:58 +0300

    feat: add event, ticket, and user services with CRUD functionality and DTOs

--- services/event-service/src/app.module.ts
+++ services/event-service/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventModule } from './event.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [EventModule],
+  imports: [EventModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- services/ticket-service/src/app.module.ts
+++ services/ticket-service/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TicketModule } from './ticket.module';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
   //the reason why is
-  imports: [TicketModule],
+  imports: [TicketModule, TicketsModule],
   controllers: [AppController],
   providers: [AppService],
 })
