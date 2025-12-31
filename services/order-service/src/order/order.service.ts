3408fce15840e4907b5227d09e1a26ceccbd3a7b
Melisaates <101855839+Melisaates@users.noreply.github.com>
2025-12-31 15:06:15 +0300

    feat(order-service): add ticketId to CreateOrderDto and implement order creation with Kafka message publishing

--- services/order-service/src/order/order.service.ts
+++ services/order-service/src/order/order.service.ts
@@ -1,11 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Kafka } from 'kafkajs';
 
 @Injectable()
 export class OrderService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(
+    private kafkaProducer: KafkaProducer,
+    private prisma: PrismaService,
+    private http: HttpService,
+
+  ) {}
+  async createOrder(userId: string, createOrderDto: CreateOrderDto) {
+    const ticketResponse = await this.http.get(`http://ticket-service/tickets/${createOrderDto.ticketId}`).toPromise();
+    const {price} = ticketResponse.data;
+
+    await this.http.post(`http://ticket-service/tickets/${createOrderDto.ticketId}/reserve`).toPromise();
+
+    const order = await this.prisma.order.create({
+      data: {
+        userId, 
+        ticketId: createOrderDto.ticketId,
+        price,
+        status: 'CONFIRMED',
+      },
+    });
+
+    await this.kafkaProducer.sendMessage('order.created', {
+      orderId: order.id,
+      userId: order.userId,
+      ticketId: createOrderDto.ticketId,
+      price: order.price,
+      status: order.status,
+    });
+
+
+    return order;
   }
 
   findAll() {
