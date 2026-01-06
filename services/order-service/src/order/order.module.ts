12dadc9d5a2ade737ac3149b7ddc158df7d91478
Melisaates <101855839+Melisaates@users.noreply.github.com>
2026-01-06 12:47:20 +0300

    refactor: update Dockerfiles for all services to use node:18-bullseye, streamline npm install, and adjust CMD paths

--- services/order-service/src/order/order.module.ts
+++ services/order-service/src/order/order.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { Kafka } from 'kafkajs';
+import { KafkaModule } from 'src/kafka/kafka.module';
 
 @Module({
+  imports: [KafkaModule],
   controllers: [OrderController],
   providers: [OrderService],
 })
